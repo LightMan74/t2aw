6aa168a6470a1a0ff75455ecc6beb9a62ff046be
LightMan74 <williamlansard@gmail.com>
2026-07-11 19:18:30 +0200

    2026-07-11--19-18

--- generation.php
+++ generation.php
@@ -18,7 +18,6 @@
 include "api/check_connected.php";
 include 'menu.php'; ?>
         </nav>
-
         <h1>Génération de l'ordre des matchs de poule</h1>
 
         <!-- <label>ID Tournoi :</label> -->
@@ -26,15 +25,18 @@ include 'menu.php'; ?>
         <!-- <input type="number" id="id_tournoi" value="1" style="width:100px; margin-bottom:15px;"> -->
         <button onclick="chargerMatchs()" hidden>Charger / Générer l'ordre</button>
 
-
-
         <h2>Ordre proposé (glisser-déposer pour réorganiser)</h2>
-        <div id="liste-matchs"></div>
 
-        <div id="message"></div>
-        <button onclick="validerOrdre()" style="margin-top:15px;">Valider et enregistrer les matchs</button>
+        <center>
+            <div style="max-width:30%">
+                <div id="liste-matchs"></div>
+
+                <div id="message"></div>
+                <button onclick="validerOrdre()" style="margin-top:15px;">Valider et enregistrer les matchs</button>
 
 
+            </div>
+        </center>
     </div>
 
     <script src="js/generation.js"></script>
