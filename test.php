faae66ac86d8169e0b5f1556f435977e0f0dff31
LightMan74 <williamlansard@gmail.com>
2026-07-15 18:04:24 +0200

    2026-07-15--18-04

--- test.php
+++ test.php
@@ -1,146 +1,75 @@
-<!DOCTYPE html>
-<html>
+<script>
+// console.log("2 -> " + Math.log2(2));
+// console.log("4 -> " + Math.log2(4));
+// console.log("8 -> " + Math.log2(8));
+// console.log("16 -> " + Math.log2(16));
+// console.log("32 -> " + Math.log2(32));
+// console.log("5 -> " + Math.log2(5));
+// console.log("5R -> " + Math.round(Math.log2(5)));
+// console.log("6 -> " + Math.log2(6));
+// console.log("6R -> " + Math.round(Math.log2(6)));
+// console.log("7 -> " + Math.log2(7));
+// console.log("7 -> " + Math.round(Math.log2(7)));
+// console.log("12 -> " + Math.log2(12));
+// console.log("12 -> " + Math.round(Math.log2(12)));
+function forcerPuissanceDe2(nbre_team) {
+    return Math.pow(2, Math.ceil(Math.log2(nbre_team)));
+}
 
-<head>
-    <meta charset="UTF-8">
-    <title>Test Bracket Generator</title>
-</head>
 
-<body>
-    <pre id="output"></pre>
-
-    <script src="js/bracket-generator.js"></script>
-    <script>
-    const teams = [1, 2, 3, 4, 5, 6];
-    const bracket = BracketGenerator.generateBracket(teams);
-
-    let log = '';
-    log += 'Total rounds: ' + bracket.totalRounds + '\n';
-    log += 'Ranking ranges: ' + JSON.stringify(bracket.rankingRanges, null, 2) + '\n\n';
-
-    log += 'Winner Round 1 matches:\n';
-    bracket.winnerRounds[0].matches.forEach(m => {
-        log += `  ${m.id}: T${m.team1} vs T${m.team2} (bye: ${m.isBye})\n`;
-    });
-
-    log += '\nWinner Round 2 matches:\n';
-    bracket.winnerRounds[1].matches.forEach(m => {
-        log += `  ${m.id}: team1From=${m.team1From} vs team2From=${m.team2From}\n`;
-    });
-
-    log += '\n=== LOSER BRACKETS ===\n';
-    Object.keys(bracket.loserBrackets).forEach(r => {
-        log += `\n-- Round ${r} (loser group) --\n`;
-        const lb = bracket.loserBrackets[r];
-        lb.rounds.forEach(sr => {
-            log += `  SubRound ${sr.subRound}:\n`;
-            sr.matches.forEach(m => {
-                if (m.isPlacementMatch) {
-                    log += `    [PLACEMENT] ${m.id}: loserOf(${m.team1FromSubLoser}) vs loserOf(${m.team2FromSubLoser})\n`;
-                } else {
-                    log += `    ${m.id}: from=${m.team1From || m.team1FromSub} vs from=${m.team2From || m.team2FromSub} (bye: ${m.isBye})\n`;
-                }
-            });
-        });
-        log += `  Placement matches -> ranks:\n`;
-        lb.placementMatches.forEach(pm => {
-            log += `    ${pm.matchId}: winner=rank${pm.winnerRank}, loser=rank${pm.loserRank}\n`;
-        });
-    });
-
-    document.getElementById('output').textContent = log;
-    </script>
-</body>
-
-</html>
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="UTF-8">
-    <title>Test Bracket Resolver</title>
-</head>
-
-<body>
-    <pre id="output"></pre>
-
-    <!-- <script src="js/bracket-generator.js"></script> -->
-    <script src="js/bracket-resolver.js"></script>
-    <script>
-    // const teams = [1, 2, 3, 4, 5, 6];
-    // const bracket = BracketGenerator.generateBracket(teams);
-    const state = BracketResolver.initMatchState(bracket);
-
-    // let log = '';
-
-    function playAndLog(matchId, winner) {
-        BracketResolver.resolveMatch(bracket, state, matchId, winner);
-        log += `Match ${matchId} joué -> gagnant: T${winner}\n`;
+function subroundcalc(nbre_team) {
+    nbre_team = forcerPuissanceDe2(nbre_team);
+    let nbre_round = Math.round(Math.log2(nbre_team));
+    const array_round = [];
+    let cnr = 0;
+    let fnr = nbre_team;
+    array_round[0] = [];
+    array_round[0][0] = fnr;
+    array_round[0][1] = nbre_team / fnr;
+    for (let i = 1; i < nbre_round; i++) {
+        cnr++;
+        fnr = fnr / 2;
+        array_round[cnr] = [];
+        array_round[cnr][0] = fnr;
+        array_round[cnr][1] = nbre_team / fnr;
     }
-    // playAndLog('WR1_M1', 1);
-    playAndLog('WR1_M2', 4);
-    // playAndLog('WR1_M3', 4);
-    playAndLog('WR1_M4', 3);
+    return array_round;
+}
 
+function listematchph(nbre_team) {
+    array_round = subroundcalc(nbre_team);
+    console.table(array_round);
+    console.log(array_round.length);
+    console.log("ROUND: 0");
+    console.log("ROUND: 0 - SUB: 1");
+    for (let i = 1; i <= array_round[0][0] / 2; i++) {
+        console.log("match: R0_S1_M" + i + " - T" + i + " vs T" + (9 - i));
+    }
+    for (let j = 1; j < array_round.length; j++) {
+        console.log("ROUND: " + j);
+        let classementpalce = 1;
+        for (let k = 1; k <= array_round[j][1]; k++) {
+            let place = "";
+            if (j == array_round.length - 1) {
+                place = " - CLASSMENT " + classementpalce++ + ' - ' + classementpalce++;
+            }
+            console.log("ROUND: " + j + " - SUB: " + k + place);
+            let wl;
+            if (k % 2 == 0) {
+                wl = "Loss_";
+            } else {
+                wl = "Win_";
+            }
+            let mplus = 0;
+            let splus = Math.ceil((k / 2));
+            for (let i = 1; i <= array_round[j][0] / 2; i++) {
+                console.log("match: R" + j + "_S" + k + "_M" + i + "__" + wl + "R" + (j - 1) + "_S" + splus + "_M" + (i + mplus) + '_vs_' + wl + "R" + (j - 1) + "_S" + splus + "_M" + (i + ++mplus));
 
-    log += '\n-- Playable après Round 1 --\n';
-    BracketResolver.getPlayableMatches(bracket, state).forEach(m => {
-        log += `  ${m.id}: T${m.team1} vs T${m.team2}\n`;
-    });
+            }
+        }
+    }
+}
 
-    playAndLog('WR3_M1', 1);
-    playAndLog('LR1_S1_M1', 4);
-    // playAndLog('WR2_M3', 2);
-    // playAndLog('WR2_M4', 3);
-    // playAndLog('LR1_S1_M1', 9);
-    // playAndLog('LR1_S1_M2', 12);
-    // playAndLog('LR1_S1_M3', 10);
-    // playAndLog('LR1_S1_M4', 11);
-
-    // // Winner Round 2 (demi-finales)
-    // playAndLog('WR2M5', 1); // T1 bat T4
-    // playAndLog('WR2M6', 2); // T2 bat T3
-
-    // Loser bracket round 1(perdants R1: T8, T5, T7, T6)
-    log += '\n-- Playable après demi-finales --\n';
-    BracketResolver.getPlayableMatches(bracket, state).forEach(m => {
-        log += `  ${m.id}: T${m.team1} vs T${m.team2}\n`;
-    });
-
-    // playAndLog('LR1S1M1', 5); // T5 bat T8
-    // playAndLog('LR1S1M2', 6); // T6 bat T7
-
-    log += '\n-- Playable après LR1 sub-round 1 --\n';
-    BracketResolver.getPlayableMatches(bracket, state).forEach(m => {
-        log += `  ${m.id}: T${m.team1} vs T${m.team2}\n`;
-    });
-
-    // playAndLog('LR1S2M3', 5); // T5 bat T6 -> place 5-6
-    // playAndLog('LR1PLACEMENT', 8); // T8 bat T7 -> place 7-8
-
-    // Loser bracket round 2 (perdants demi-finales: T4, T3) -> place 3-4
-    // playAndLog('LR2S1M1', 4); // T4 bat T3
-
-    // Finale winner bracket -> place 1-2
-    log += '\n-- Match final --\n';
-    BracketResolver.getPlayableMatches(bracket, state).forEach(m => {
-        log += `  ${m.id}: T${m.team1} vs T${m.team2}\n`;
-    });
-
-    // // On cherche le match final (dernier round winner)
-    // const finalRound = bracket.winnerRounds[bracket.winnerRounds.length - 1];
-    // const finalMatchId = finalRound.matches[0].id;
-    // // playAndLog(finalMatchId, 1); // T1 bat T2
-    // playAndLog('WR3M7', 1); // T1 bat T2
-
-    log += '\n=== CLASSEMENT FINAL ===\n';
-    BracketResolver.getFinalRanking(state).forEach(r => {
-        log += `  Rank ${r.rank}: Team ${r.team}\n`;
-    });
-
-    document.getElementById('output').textContent = log;
-    </script>
-</body>
-
-</html>
+// console.table(subroundcalc(8));
+listematchph(8);
+</script>
